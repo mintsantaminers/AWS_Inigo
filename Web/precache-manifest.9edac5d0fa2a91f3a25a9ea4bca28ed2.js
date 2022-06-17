3db7875dca501504aa03fd0a2e9965ab2d4e5c13
Santa Miners <96439368+mintsantaminers@users.noreply.github.com>
2022-06-17 14:04:32 +0200

    Add files via upload

---
+++ Web/precache-manifest.9edac5d0fa2a91f3a25a9ea4bca28ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163004f34cab3e875a3ee6d256e4df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "859aa9c23f54dd3ed8ed",
+    "url": "/static/css/main.b18086fc.chunk.css"
+  },
+  {
+    "revision": "ea4cffeb07a6e271f7ba",
+    "url": "/static/js/2.249ce2f1.chunk.js"
+  },
+  {
+    "revision": "2b71185ca4129672a57a1a5686075857",
+    "url": "/static/js/2.249ce2f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859aa9c23f54dd3ed8ed",
+    "url": "/static/js/main.5e52cae5.chunk.js"
+  },
+  {
+    "revision": "4642f384fd1c21850c58",
+    "url": "/static/js/runtime-main.57eb0afe.js"
+  }
+]);
